5e26a5c3a345afe53741b1c61f6b25e5804dd90d
Roshan <1roshanekka@gmail.com>
2024-07-16 23:35:54 +0530

    Initial Commit

---
+++ ProPlan/ProPlanApp.swift
@@ -0,0 +1,32 @@
+//
+//  ProPlanApp.swift
+//  ProPlan
+//
+//  Created by Roshan Ekka on 16/07/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ProPlanApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
